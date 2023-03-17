faff1f40f2c638d86be98cc46d963a1b75a2445c
moahmed amine mezghich <git@mezghich.com>
2023-03-17 13:10:10 +0100

    add et list de country

--- index.js
+++ index.js
@@ -12,8 +12,47 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-// mettre le serveur à l'écoute(en marche) sur le port 85
+// 1) mettre le serveur à l'écoute(en marche) sur le port 85
 app.listen(
     83,
     ()=>{console.log("Serveur Express a l ecoute sur le port 83");}
 );
+
+// 2) connexion de notre serveur à la base mongo
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const dbName = 'monapi';
+
+
+let db 
+
+
+MongoClient.connect(url, function(err, client) {
+ console.log("Connexion réussi avec Mongo");
+ db = client.db(dbName);
+});
+
+// 3) Création api (endpoints)
+
+// 3.1)Get All countries
+app.get('/countries', (req,res) => {
+      db.collection('country').find({}).toArray(function(err, data) {
+          if (err) {
+              console.log(err)
+              throw err
+          }
+          res.status(200).json(data)
+        }) 
+    })
+
+//3.2) Post new Country
+app.post('/countries', async (req,res) => {
+      try {
+          const countryData = req.body
+          const country = await db.collection('country').insertOne(countryData)
+          res.status(201).json(country)
+      } catch (err) {
+          console.log(err)
+          throw err
+      }
+    })
