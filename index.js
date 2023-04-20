7e975af8ed53584612a078f01b5dc03186813e89
moahmed amine mezghich <git@mezghich.com>
2023-04-20 09:42:25 +0100

    backend mongodb_ExpressJS + jwt

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 
-const jwt           = require('jsonwebtoken');
-var expressJWT      = require('express-jwt');
+const jwt = require('jsonwebtoken');
+var expressJWT = require('express-jwt');
 
 app.use(express.json());
 
@@ -11,48 +11,52 @@ app.use(express.json());
 var corsOptions = {
     //origin: "http://localhost:4200"
     origin: "*"
-  };
+};
 
 app.use(cors(corsOptions));
 
 // 1) mettre le serveur à l'écoute(en marche) sur le port 85
 app.listen(
     83,
-    ()=>{console.log("Serveur Express a l ecoute sur le port 83");}
+    () => { console.log("Serveur Express a l ecoute sur le port 83"); }
 );
 
 // 2) connexion de notre serveur à la base mongo
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017';
-const dbName = 'monapi';
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const dbName = 'monapi';
 
 
-let db 
+let db
 
 
-MongoClient.connect(url, function(err, client) {
- console.log("Connexion réussi avec Mongo");
- db = client.db(dbName);
+MongoClient.connect(url, function (err, client) {
+    console.log("Connexion réussi avec Mongo DB et compass");
+    db = client.db(dbName);
 });
 
 
 
-    //4) Partie JWT
-    // SECRET FOR JWT
-let secret = 'gk2023'; // a secret key is set here
+//     //4) Partie JWT
+//     // SECRET FOR JWT
+let secret = 'plb2023'; // a secret key is set here
 
 /* Create token to be used */
-
-/* Create token to be used */
-app.get('/token/sign', (req, res) => {
+app.post('/token/sign', (req, res) => {
+    let credentials =  req.body;
+    if((credentials.email==="amine.mezghich@gmail.com") && (credentials.password==="1234")){
     // vérifier si un user admet le login et le mot de passe
     var userData = {
         "name": "Amine Mezghich",
         "id": "1234",
+        "Poste actuel":"Formateur Senior en IT",
         "role":"Chef de projet"
     }
-    let token = jwt.sign(userData, secret, { expiresIn: '10000s'})
-    res.status(200).json({"token": token});
+        let token = jwt.sign(userData, secret, { expiresIn: '10000s'})
+        res.status(200).json({"token": token});
+    }
+    else
+       res.status(401).json({"Message": "Invalide login/password"});
 });
 
 app.use(expressJWT({ secret: secret, algorithms: ['HS256']})
@@ -63,27 +67,27 @@ app.use(expressJWT({ secret: secret, algorithms: ['HS256']})
         ]}
     ));
 
-// 3) Création api (endpoints)
+// // 3) Création api (endpoints)
 
-// 3.1)Get All countries
-app.get('/countries', (req,res) => {
-      db.collection('country').find({}).toArray(function(err, data) {
-          if (err) {
-              console.log(err)
-              throw err
-          }
-          res.status(200).json(data)
-        }) 
+// // 3.1)Get All countries
+app.get('/countries', (req, res) => {
+    db.collection('country').find({}).toArray(function (err, data) {
+        if (err) {
+            console.log(err)
+            throwerr
+        }
+        res.status(200).json(data)
     })
+})
 
-//3.2) Post new Country
-app.post('/countries', async (req,res) => {
-      try {
-          const countryData = req.body
-          const country = await db.collection('country').insertOne(countryData)
-          res.status(201).json(country)
-      } catch (err) {
-          console.log(err)
-          throw err
-      }
-    })
+// //3.2) Post new Country
+app.post('/countries', async (req, res) => {
+    try {
+        const countryData = req.body
+        const country = await db.collection('country').insertOne(countryData)
+        res.status(201).json(country)
+    } catch (err) {
+        console.log(err)
+        throw err
+    }
+})
